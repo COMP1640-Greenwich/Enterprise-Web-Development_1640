dc5c4325c88b32496eea8f9efe2be38e90511dd8
noelisme <155471187+noelisme@users.noreply.github.com>
2024-03-12 16:28:21 +0700

    adding delete edit reset password function in Admin page

---
+++ 1640/Models/VM/ResetPasswordVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _1640.Models.VM
+{
+    public class ResetPasswordVM
+    {
+        [Required][EmailAddress] public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirm password must match")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
