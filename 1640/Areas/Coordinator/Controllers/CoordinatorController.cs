f587d8e0ef8a1ebdebce285e1fbbbde59bc4ebe1
Vu Tien Thanh <vutienthanh25092003@gmail.com>
2024-03-12 16:34:36 -0700

    create view and controller to display Coordinator

---
+++ 1640/Areas/Coordinator/Controllers/CoordinatorController.cs
@@ -0,0 +1,30 @@
+﻿using _1640.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1640.Areas.Coordinator.Controllers
+{
+    [Area("Coordinator")]
+    public class CoordinatorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+    public CoordinatorController(ApplicationDbContext db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+      
+    }
+}
