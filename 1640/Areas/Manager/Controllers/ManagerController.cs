cdd10bb19f105b27d25edd2d064fa3f757498e92
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2024-03-12 16:53:18 +0700

    tạo area Manager

---
+++ 1640/Areas/Manager/Controllers/ManagerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1640.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
