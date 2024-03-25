9d97e2e85ff5ba7610c3efdce88778eac0a28a44
ADMIN <ADMIN@MSI>
2024-03-25 23:28:35 +0700

    Manager download ZIP file.

--- 1640/Areas/Manager/Controllers/ManagerController.cs
+++ 1640/Areas/Manager/Controllers/ManagerController.cs
@@ -1,6 +1,7 @@
 ﻿using _1640.Areas.Repository.IRepository;
 using _1640.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
 
 namespace _1640.Areas.Manager.Controllers
 {
@@ -13,14 +14,57 @@ namespace _1640.Areas.Manager.Controllers
         //    _db = db;
         //}
         private readonly IUnitOfWork _unitOfWork;
-        public ManagerController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ManagerController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
         }
         public IActionResult Index()
         {
             List<Faculty> faculities = _unitOfWork.FacultyRepository.GetAll().ToList();
             return View(faculities);
+        }
+
+        public IActionResult List()
+        {
+            List<Article> articles = _unitOfWork.ArticleRepository.GetAll().ToList();
+            return View(articles);
+        }
+
+        public IActionResult DownloadDocInZip(int id)
+        {
+            var article = _unitOfWork.ArticleRepository.Get(a => a.Id == id);
+            if (article == null || (string.IsNullOrEmpty(article.DocxUrl) && string.IsNullOrEmpty(article.ImageUrl)))
+            {
+                return NotFound();
+            }
+
+            var zipPath = Path.GetTempFileName() + ".zip";
+            using (var zip = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            {
+                if (!string.IsNullOrEmpty(article.DocxUrl))
+                {
+                    var docPath = Path.Combine(_hostingEnvironment.WebRootPath, article.DocxUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(docPath))
+                    {
+                        zip.CreateEntryFromFile(docPath, Path.GetFileName(docPath));
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(article.ImageUrl))
+                {
+                    var imagePath = Path.Combine(_hostingEnvironment.WebRootPath, article.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        zip.CreateEntryFromFile(imagePath, Path.GetFileName(imagePath));
+                    }
+                }
+            }
+
+            var zipBytes = System.IO.File.ReadAllBytes(zipPath);
+            return File(zipBytes, "application/zip", "doc.zip");
         }
 
         public IActionResult Create()
