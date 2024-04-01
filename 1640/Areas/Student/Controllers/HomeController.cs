717bedb043f3f48fb112893facb4080d822ac565
Khánh Phùng <noellll.2003@gmail.com>
2024-04-01 23:30:04 +0700

    Search Function and Pagination in Home and Index in Manager.

--- 1640/Areas/Student/Controllers/HomeController.cs
+++ 1640/Areas/Student/Controllers/HomeController.cs
@@ -9,23 +9,35 @@ using System.Diagnostics;
 namespace _1640.Areas.Student.Controllers
 {
     [Area("Student")]
-	public class HomeController : Controller
+    public class HomeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
 
         private readonly ILogger<HomeController> _logger;
+		private readonly int _recordsPerPage = 4;
 
-        public HomeController(IUnitOfWork unitOfWork, ILogger<HomeController> logger)
+		public HomeController(IUnitOfWork unitOfWork, ILogger<HomeController> logger)
         {
             _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int id, string searchString = "")
         {
-            List<Article> articles = _unitOfWork.ArticleRepository.GetAllApprove("Semester").ToList();
-            return View(articles);
-        }
+            List<Article> articles = _unitOfWork.ArticleRepository.GetAllApprove("Semester")
+                .Where(b => b.Title.Contains(searchString))
+                .ToList();
+
+			int numberOfRecords = articles.Count();
+			int numberOfPages = (int)Math.Ceiling((double)numberOfRecords / _recordsPerPage);
+
+			ViewBag.numberOfPages = numberOfPages;
+			ViewBag.currentPage = id;
+			ViewData["Current Filter"] = searchString;
+
+			var articlesList = articles.Skip(id * numberOfPages).Take(_recordsPerPage).ToList();
+			return View(articlesList);
+		}
 
         public IActionResult Privacy()
         {
@@ -36,6 +48,10 @@ namespace _1640.Areas.Student.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Login()
+        {
+            return Redirect("Identity/Account/Login");
         }
     }
 }
